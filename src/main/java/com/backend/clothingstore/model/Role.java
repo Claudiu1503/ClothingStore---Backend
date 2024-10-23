db92da5e4da09be770d52262bec401909235e6ad
Claudiu_1503 <adichis1503@yahoo.com>
2024-10-23 15:14:59 +0300

    Adding user model

---
+++ src/main/java/com/backend/clothingstore/model/Role.java
@@ -0,0 +1,6 @@
+package com.backend.clothingstore.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- src/main/java/com/backend/clothingstore/model/User.java
+++ src/main/java/com/backend/clothingstore/model/User.java
@@ -1,5 +1,74 @@
 package com.backend.clothingstore.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "users")
 public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+
+  @NonNull
+  @NotBlank(message = "Name is required")
+  private String username;
+
+  private String firstName;
+  private String lastName;
+
+  @NonNull
+  @Email(message = "Please provide a valid email address")
+  @NotBlank(message = "Email is required")
+  @Column(unique = true)
+  private String email;
+
+
+  @NonNull
+  @NotBlank(message = "Password is required")
+  private String password;
+
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private Role role;
+
+  private String phone;
+  private String address;
+  private String city;
+  private String state;
+  private String zip;
+  private String country;
+
+
+  private Boolean isVerified;
+
+  private String verificationToken;
+
+  private LocalDateTime createdAt;
+
+  private LocalDateTime updatedAt;
+
+  @PrePersist
+  public void prePersist() {
+    createdAt = LocalDateTime.now();
+  }
+
+  @PreUpdate
+  public void preUpdate() {
+    updatedAt = LocalDateTime.now();
+  }
+
 
 }
