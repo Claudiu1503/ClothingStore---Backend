c0d6bfd8e787b2d329594db9734bba23f2f6ba38
Claudiu_1503 <adichis1503@yahoo.com>
2024-10-23 13:40:45 +0300

    Explicat la andrei

--- src/main/java/com/backend/clothingstore/model/User.java
+++ src/main/java/com/backend/clothingstore/model/User.java
@@ -1,11 +1,5 @@
 package com.backend.clothingstore.model;
 
 public class User {
-    //private int mata
-    //private string ma-ta*
-    //ma cac pe el proiect
-    //o sa fie confict
-    private int ideeeeeee;
-    private int id;
-    private String username;
+
 }
