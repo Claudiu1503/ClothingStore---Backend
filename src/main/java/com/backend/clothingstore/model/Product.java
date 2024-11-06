8201c87e35eecf6b992f4aaafca43ec5ee01b92b
Claudiu_1503 <adichis1503@yahoo.com>
2024-11-06 21:27:20 +0200

    Normalize brand names in Brand enum
    Standardized the format of brand names in the Brand enum to remove special characters and spaces. This change also reduces the length of the brand column in the Product entity from 600 to 512 characters for optimization.

--- src/main/java/com/backend/clothingstore/model/Product.java
+++ src/main/java/com/backend/clothingstore/model/Product.java
@@ -35,7 +35,7 @@ public class Product {
     private ProductColor color;
 
     @Enumerated(EnumType.STRING)
-    @Column(length = 600)
+    @Column(length = 512)
     private Brand brand;
 
     @Column(nullable = false)
